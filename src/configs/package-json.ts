ddfab23b2417e90ba666d95768d1d3ac8c499754
Christopher Buss <christopher.buss+github@protonmail.com>
2025-08-01 23:12:12 +0100

    feat(package-json): add additional validation rules (#364)
    * feat(package-json): add additional validation rules
    
    * fix(ci): update node.js version from 22.0.0 to 22.1.0

--- src/configs/package-json.ts
+++ src/configs/package-json.ts
@@ -2,9 +2,9 @@ import type { OptionsProjectType, TypedFlatConfigItem } from "../types";
 import { interopDefault } from "../utils";
 
 export async function packageJson(
-	options: OptionsProjectType = {},
+	options: OptionsProjectType & { roblox?: boolean } = {},
 ): Promise<Array<TypedFlatConfigItem>> {
-	const { type = "game" } = options;
+	const { roblox = true, type = "game" } = options;
 
 	const [jsoncEslintParser, pluginPackageJson] = await Promise.all([
 		interopDefault(import("jsonc-eslint-parser")),
@@ -26,22 +26,37 @@ export async function packageJson(
 				"package-json/no-redundant-files": "error",
 				"package-json/order-properties": "error",
 				"package-json/repository-shorthand": "error",
+				"package-json/require-type": "error",
 				"package-json/sort-collections": "error",
 				"package-json/unique-dependencies": "error",
-				"package-json/valid-local-dependency": "error",
+				"package-json/valid-author": "error",
+				"package-json/valid-bin": "error",
+				"package-json/valid-bundleDependencies": "error",
+				"package-json/valid-config": "error",
+				"package-json/valid-cpu": "error",
+				"package-json/valid-license": "error",
 				"package-json/valid-name": "error",
 				"package-json/valid-package-definition": "error",
 				"package-json/valid-repository-directory": "error",
+				"package-json/valid-scripts": "error",
+				"package-json/valid-type": "error",
 				"package-json/valid-version": "error",
 
 				...(type === "package"
 					? {
 							"package-json/require-author": "error",
+							"package-json/require-description": "error",
 							"package-json/require-files": "error",
 							"package-json/require-keywords": "error",
 							"package-json/require-name": "error",
 							"package-json/require-types": "error",
 							"package-json/require-version": "error",
+						}
+					: {}),
+
+				...(type === "package" && roblox === false
+					? {
+							"package-json/require-engines": "error",
 						}
 					: {}),
 			},
